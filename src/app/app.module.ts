2295cd7dfd969fce0639f7d0bf7d9f1c85b64102
saidtalfana <saidtalfana1@gmail.com>
2024-08-11 12:46:15 +0100

    models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,55 +3,49 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignupComponent } from './component/signup/signup.component';
-import { SigninComponent } from './component/signin/signin.component';
-import { DashboardComponent } from './component/dashboard/dashboard.component';
-import { UserComponent } from './component/user/user.component';
-import { HomeComponent } from './component/home/home.component';
-import { AddUserComponent } from './Component/Dashboard/add-user/add-user.component';
-import { AddTechnicianComponent } from './Component/Dashboard/add-technician/add-technician.component';
+import { SigninComponent } from './Component/Main/signin/signin.component';
 import { AddEquipmentComponent } from './Component/Dashboard/add-equipment/add-equipment.component';
 import { AddFailureComponent } from './Component/Dashboard/add-failure/add-failure.component';
-import { ShowUserComponent } from './Component/Dashboard/show-user/show-user.component';
-import { ShowTechnicianComponent } from './Component/Dashboard/show-technician/show-technician.component';
-import { ShowEquipmentComponent } from './Component/Dashboard/show-equipment/show-equipment.component';
+import { AddTechnicianComponent } from './Component/Dashboard/add-technician/add-technician.component';
+import { AddUserComponent } from './Component/Dashboard/add-user/add-user.component';
 import { ChartComponent } from './Component/Dashboard/chart/chart.component';
+import { DashboardComponent } from './Component/Dashboard/dashboard/dashboard.component';
 import { LogoutComponent } from './Component/Dashboard/logout/logout.component';
-import { HeaderComponent } from './Component/Main/header/header.component';
-import { MainComponent } from './Component/Main/main/main.component';
+import { ShowUserComponent } from './Component/Dashboard/show-user/show-user.component';
+import { ShowEquipmentComponent } from './Component/Dashboard/show-equipment/show-equipment.component';
+import { ShowTechnicianComponent } from './Component/Dashboard/show-technician/show-technician.component';
 import { ShowTicketComponent } from './Component/Technician/show-ticket/show-ticket.component';
-import { UpdateTicketComponent } from './Component/Technician/update-ticket/update-ticket.component';
 import { TechnicianComponent } from './Component/Technician/technician/technician.component';
+import { UpdateTicketComponent } from './Component/Technician/update-ticket/update-ticket.component';
 import { AddTicketComponent } from './Component/User/add-ticket/add-ticket.component';
+import { UserComponent } from './Component/User/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SignupComponent,
-    SigninComponent,
-    DashboardComponent,
-    UserComponent,
-    HomeComponent,
-    AddUserComponent,
-    AddTechnicianComponent,
-    AddEquipmentComponent,
-    AddFailureComponent,
-    ShowUserComponent,
-    ShowTechnicianComponent,
-    ShowEquipmentComponent,
-    ChartComponent,
-    LogoutComponent,
-    HeaderComponent,
-    MainComponent,
-    ShowTicketComponent,
-    UpdateTicketComponent,
-    TechnicianComponent,
-    AddTicketComponent
+      AppComponent,
+      SigninComponent,
+      SigninComponent,
+      AddEquipmentComponent,
+      AddFailureComponent,
+      AddTechnicianComponent,
+      AddUserComponent,
+      ChartComponent,
+      DashboardComponent,
+      LogoutComponent,
+      ShowEquipmentComponent,
+      ShowTechnicianComponent,
+      ShowUserComponent,
+      ShowTicketComponent,
+      TechnicianComponent,
+      UpdateTicketComponent,
+      AddTicketComponent,
+      ShowTicketComponent,
+      UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
